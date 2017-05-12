aed4f3f7a0aed0d8286f3b9558db338aa36ba5a7
Nicholas Mead <nixter11@uw.edu>
2017-05-12 14:07:31 -0700

    finish implementing api

---
+++ src/Portfolio/Models/Project.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Portfolio.Models
+{
+    public class Project
+    {
+        public string html_url { get; set; }
+        public string name { get; set; }
+        public int stargazers_count { get; set; }
+        public int size { get; set; }
+
+        public static List<Project> GetProjects()
+        {
+            var client = new RestClient("https://api.github.com/");
+            var request = new RestRequest("users/NMead93/starred", Method.GET);
+            request.AddHeader("User-Agent", "NMead93");
+            request.AddParameter("type", "all");
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(response.Content);
+            var projectList = JsonConvert.DeserializeObject<List<Project>>(jsonResponse.ToString());
+            return projectList;
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
